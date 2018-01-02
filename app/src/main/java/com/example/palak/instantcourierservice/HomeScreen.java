12cdb5aee90e1b248493a553984609aa98822c87
Palak Jain <palakjain444@gmail.com>
2018-01-03 00:59:41 +0530

    flash screen made

---
+++ app/src/main/java/com/example/palak/instantcourierservice/HomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.palak.instantcourierservice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeScreen extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_home);
+    }
+}
