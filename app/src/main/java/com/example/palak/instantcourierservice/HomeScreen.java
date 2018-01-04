4bf2f7d96d3572888e38138c1cb6d3f94a6c4ead
Palak Jain <palakjain444@gmail.com>
2018-01-05 00:21:31 +0530

    flash screen only

--- app/src/main/java/com/example/palak/instantcourierservice/HomeScreen.java
+++ app/src/main/java/com/example/palak/instantcourierservice/HomeScreen.java
@@ -1,5 +1,6 @@
 package com.example.palak.instantcourierservice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,5 +10,17 @@ public class HomeScreen extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_home);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    Thread.sleep(3 * 1000);
+                    Intent i = new Intent(HomeScreen.this, MainActivity.class);
+                    startActivity(i);
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        thread.start();
     }
 }
